b4e34b9105ba900615dad0a7fc739d413fefda19
Tracy Payne <krpsmom@hotmail.com>
2018-03-14 16:46:15 -0500

    validated tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,8 @@ class Tweet < ApplicationRecord
   belongs_to :user
   end
 
+  validates :message, presence: true
+  validates :message, length: {maximum: 140,
+too_long: "A tweet is only 140 max. Everybody knows that!"}
+
 end
